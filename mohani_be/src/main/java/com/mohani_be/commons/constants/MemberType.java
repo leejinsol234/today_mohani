82bdf4f36a15f410f71d22cd695172519d166f2d
hilim9 <dlagydls1212@naver.com>
2024-01-08 20:16:05 +0900

    config 수정, membertype 삭제

--- mohani_be/src/main/java/com/mohani_be/commons/constants/MemberType.java
+++
@@ -1,6 +0,0 @@
-package com.mohani_be.commons.constants;
-
-public enum MemberType {
-    USER,
-    ADMIN
-}
--- mohani_be/src/main/java/com/mohani_be/configs/SecurityConfig.java
+++ mohani_be/src/main/java/com/mohani_be/configs/SecurityConfig.java
@@ -1,2 +1,22 @@
-package com.mohani_be.configs;public class SecurityConfig {
+package com.mohani_be.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
