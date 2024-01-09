b56164fdba57cbf231aa3e949ab650c9b2e5e6e6
hilim9 <dlagydls1212@naver.com>
2024-01-09 17:12:10 +0900

    Member 엔티티, Member repository 설정

---
+++ mohani_be/src/main/java/com/mohani_be/entities/Member.java
@@ -0,0 +1,32 @@
+package com.mohani_be.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Member extends Base{
+
+    @Id @GeneratedValue
+    private Long memberNo;
+
+    @Column (length = 60, nullable = false, unique = true)
+    private String email;
+
+    @Column (nullable = false)
+    private String password;
+
+    @Column (nullable = false)
+    private String name;
+
+    @Column (nullable = false)
+    private String mobile;
+
+}
---
+++ mohani_be/src/main/java/com/mohani_be/repositories/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.mohani_be.repositories;
+
+import com.mohani_be.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+}
