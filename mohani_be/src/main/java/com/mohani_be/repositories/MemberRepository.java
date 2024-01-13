c6483661d7f08ce3e1666c6407bd40452ceb7142
hilim9 <dlagydls1212@naver.com>
2024-01-13 16:04:07 +0900

    MemberInfo 값 변경 MemberRepository 기능 추가 MemberInfoService 추가

--- mohani_be/src/main/java/com/mohani_be/repositories/MemberRepository.java
+++ mohani_be/src/main/java/com/mohani_be/repositories/MemberRepository.java
@@ -1,8 +1,21 @@
 package com.mohani_be.repositories;
 
 import com.mohani_be.entities.Member;
+import com.mohani_be.entities.QMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+
+    // email 조회 가능하게 추가
+    // Optional로 null값 처리
+    Optional<Member> findByEmail(String email);
+
+    // email 존재 여부 검증 처리
+    default boolean exists(String email) {
+        return exists(QMember.member.email.eq(email));
+    }
+
 }
