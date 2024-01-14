ad70e09076e4f28b5a1beafb5ce4c807fc6ebe55
hilim9 <dlagydls1212@naver.com>
2024-01-14 23:14:08 +0900

    회원가입 유효성 검사 및 예외 처리

---
+++ mohani_be/src/main/java/com/mohani_be/commons/Utils.java
@@ -0,0 +1,42 @@
+package com.mohani_be.commons;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.*;
+
+public class Utils {
+
+    private static ResourceBundle validationsBundle;
+    private static ResourceBundle errorsBundle;
+
+    static {
+        validationsBundle = ResourceBundle.getBundle("messages.validations");
+        errorsBundle = ResourceBundle.getBundle("messages.errors");
+    }
+
+    public static String getMessage(String code, String bundleType) {
+        bundleType = Objects.requireNonNullElse(bundleType, "validation");
+        ResourceBundle bundle = bundleType.equals("error")? errorsBundle:validationsBundle;
+        try {
+            return bundle.getString(code);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static Map<String, List<String>> getMessages(Errors errors) {
+
+        Map<String, List<String>> data = new HashMap<>();
+        for (FieldError error : errors.getFieldErrors()) {
+            // NotBlank, NotBlank.email, NotBlank.requestJoin.email -> 문자열이 긴것부터 적용
+            List<String> messages = Arrays.stream(error.getCodes()).sorted(Comparator.reverseOrder())
+                    .map(c -> getMessage(c, "validation"))
+                    .filter(c -> c != null)
+                    .toList();
+
+            data.put(error.getField(), messages);
+        }
+        return data;
+    }
+}
---
+++ mohani_be/src/main/java/com/mohani_be/controllers/member/JoinValidator.java
@@ -0,0 +1,53 @@
+package com.mohani_be.controllers.member;
+
+import com.mohani_be.commons.validator.PasswordValidator;
+import com.mohani_be.commons.validator.PhoneNumberValidator;
+import com.mohani_be.repositories.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class JoinValidator implements Validator, PasswordValidator, PhoneNumberValidator {
+
+    private final MemberRepository repository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestJoin.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        RequestJoin form = (RequestJoin) target;
+
+        String email = form.getEmail();
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
+        String phoneNumber = form.getPhoneNumber();
+
+        // 1. 아이디 중복 여부 체크
+        if (email != null && !email.isBlank() && repository.exists(email)) {
+            errors.rejectValue("email", "Duplicate");
+        }
+
+        // 2. 비밀번호 복잡성 체크
+        if (password != null && !password.isBlank() && (!alphaCheck(password, false) || !numberCheck(password) || !specialCharsCheck(password))) {
+            errors.rejectValue("password", "Complexity");
+        }
+
+        // 3. 비밀번호 및 비밀번호 확인 일치 여부
+        if (password != null && !password.isBlank() && confirmPassword != null && !confirmPassword.isBlank() && !password.equals(confirmPassword)) {
+            errors.rejectValue("confirmPassword", "Mismatch");
+        }
+
+        // 4. 전화번호 형식 체크
+        if (phoneNumber != null && !phoneNumber.isBlank() && !phoneNumCheck(phoneNumber)) {
+            errors.rejectValue("phone", "phone");
+        }
+
+    }
+}
