80f3122fdc0db19d32827bf91201cc66e3be289e
hilim9 <dlagydls1212@naver.com>
2024-01-28 02:30:38 +0900

    CreateUser 수정

--- mohani_fe/src/pages/CreateUser.js
+++ mohani_fe/src/pages/CreateUser.js
@@ -1,42 +1,40 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
-export default function CreateUser() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+const CreateUser = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    username: "",
+    phoneNumber: "",
+    agree: false,
+  });
+
+  const { email, password, confirmPassword, username, phoneNumber, agree } =
+    formData;
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
 
   const handleSubmit = async () => {
-    // 서버에 전송할 데이터 설정
-    const userData = {
-      email: email,
-      password: password,
-      confirmPassword: confirmPassword,
-      username: username,
-      phoneNumber: phoneNumber,
-      agree: true,
-    };
-
     try {
-      // 서버에 POST 요청 보내기
       const response = await fetch("/mohani/join", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        // 요청 본문에 필요한 데이터 전달
-        body: JSON.stringify(userData),
+        body: JSON.stringify(formData),
       });
 
-      console.log("유저데이터 이메일 : " + userData.email);
-      console.log("유저데이터 비번 : " + userData.password);
-
-      // 서버 응답 처리
       if (response.ok) {
-        // 회원가입 성공 시 로그인페이지로 이동
-        useNavigate("/");
+        navigate("/");
       } else {
         console.error("서버 요청 실패:", response.statusText);
       }
@@ -49,59 +47,36 @@ export default function CreateUser() {
     <div className="CreatePage">
       <div className="titleWrap">회원가입</div>
       <div className="contentWrap">
-        <div className="CreateInputTitle">이메일</div>
-        <div className="CreateInputWrap">
-          <input
-            type="text"
-            className="CreateInput"
-            placeholder="이메일"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="CreateInputTitle">비밀번호</div>
-        <div className="CreateInputWrap">
-          <input
-            type="text"
-            className="CreateInput"
-            placeholder="비밀번호"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="CreateInputTitle">비밀번호 확인</div>
-        <div className="CreateInputWrap">
-          <input
-            type="text"
-            className="CreateInput"
-            placeholder="비밀번호 확인"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <div className="CreateInputTitle">이름</div>
-        <div className="CreateInputWrap">
-          <input
-            type="text"
-            className="CreateInput"
-            placeholder="이름"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="CreateInputTitle">전화번호</div>
-        <div className="CreateInputWrap">
-          <input
-            type="text"
-            className="CreateInput"
-            placeholder="전화번호"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
+        {[
+          { label: "이메일", name: "email" },
+          { label: "비밀번호", name: "password" },
+          { label: "비밀번호 확인", name: "confirmPassword" },
+          { label: "이름", name: "username" },
+          { label: "전화번호", name: "phoneNumber" },
+        ].map((field) => (
+          <div key={field.name}>
+            <div className="CreateInputTitle">{field.label}</div>
+            <div className="CreateInputWrap">
+              <input
+                type="text"
+                className="CreateInput"
+                placeholder={field.label}
+                value={formData[field.name]}
+                onChange={handleChange}
+                name={field.name}
+              />
+            </div>
+          </div>
+        ))}
       </div>
       <div className="AgreeBoxWrap">
-        <input type="checkbox" className="CreateAgreeBox" />
+        <input
+          type="checkbox"
+          className="CreateAgreeBox"
+          checked={agree}
+          onChange={handleChange}
+          name="agree"
+        />
         이용약관 및 개인정보 처리방침에 동의합니다.
       </div>
       <div>
@@ -112,9 +87,11 @@ export default function CreateUser() {
       <div className="CreateFooter">
         <div className="CreateFooter1">이미 계정이 있으신가요?</div>
         <Link to={"/"} className="CreateFooter2">
-          로그인{" "}
+          로그인
         </Link>
       </div>
     </div>
   );
-}
+};
+
+export default CreateUser;
