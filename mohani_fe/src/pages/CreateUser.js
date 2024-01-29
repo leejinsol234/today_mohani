4780079a9da3cb9dc6bb47feabaa10cd82e1a00d
hilim9 <dlagydls1212@naver.com>
2024-01-29 21:42:47 +0900

    Revert "CreateUser 수정"
    This reverts commit 80f3122fdc0db19d32827bf91201cc66e3be289e.

--- mohani_fe/src/pages/CreateUser.js
+++ mohani_fe/src/pages/CreateUser.js
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
-const CreateUser = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    username: "",
-    phoneNumber: "",
-    agree: false,
-  });
-
-  const { email, password, confirmPassword, username, phoneNumber, agree } =
-    formData;
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-  };
+export default function CreateUser() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const handleSubmit = async () => {
+    // 서버에 전송할 데이터 설정
+    const userData = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      username: username,
+      phoneNumber: phoneNumber,
+      agree: true,
+    };
+
     try {
+      // 서버에 POST 요청 보내기
       const response = await fetch("/mohani/join", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        // 요청 본문에 필요한 데이터 전달
+        body: JSON.stringify(userData),
       });
 
+      console.log("유저데이터 이메일 : " + userData.email);
+      console.log("유저데이터 비번 : " + userData.password);
+
+      // 서버 응답 처리
       if (response.ok) {
-        navigate("/");
+        // 회원가입 성공 시 로그인페이지로 이동
+        useNavigate("/");
       } else {
         console.error("서버 요청 실패:", response.statusText);
       }
@@ -47,36 +49,59 @@ const CreateUser = () => {
     <div className="CreatePage">
       <div className="titleWrap">회원가입</div>
       <div className="contentWrap">
-        {[
-          { label: "이메일", name: "email" },
-          { label: "비밀번호", name: "password" },
-          { label: "비밀번호 확인", name: "confirmPassword" },
-          { label: "이름", name: "username" },
-          { label: "전화번호", name: "phoneNumber" },
-        ].map((field) => (
-          <div key={field.name}>
-            <div className="CreateInputTitle">{field.label}</div>
-            <div className="CreateInputWrap">
-              <input
-                type="text"
-                className="CreateInput"
-                placeholder={field.label}
-                value={formData[field.name]}
-                onChange={handleChange}
-                name={field.name}
-              />
-            </div>
-          </div>
-        ))}
+        <div className="CreateInputTitle">이메일</div>
+        <div className="CreateInputWrap">
+          <input
+            type="text"
+            className="CreateInput"
+            placeholder="이메일"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="CreateInputTitle">비밀번호</div>
+        <div className="CreateInputWrap">
+          <input
+            type="text"
+            className="CreateInput"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="CreateInputTitle">비밀번호 확인</div>
+        <div className="CreateInputWrap">
+          <input
+            type="text"
+            className="CreateInput"
+            placeholder="비밀번호 확인"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div className="CreateInputTitle">이름</div>
+        <div className="CreateInputWrap">
+          <input
+            type="text"
+            className="CreateInput"
+            placeholder="이름"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="CreateInputTitle">전화번호</div>
+        <div className="CreateInputWrap">
+          <input
+            type="text"
+            className="CreateInput"
+            placeholder="전화번호"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
       </div>
       <div className="AgreeBoxWrap">
-        <input
-          type="checkbox"
-          className="CreateAgreeBox"
-          checked={agree}
-          onChange={handleChange}
-          name="agree"
-        />
+        <input type="checkbox" className="CreateAgreeBox" />
         이용약관 및 개인정보 처리방침에 동의합니다.
       </div>
       <div>
@@ -87,11 +112,9 @@ const CreateUser = () => {
       <div className="CreateFooter">
         <div className="CreateFooter1">이미 계정이 있으신가요?</div>
         <Link to={"/"} className="CreateFooter2">
-          로그인
+          로그인{" "}
         </Link>
       </div>
     </div>
   );
-};
-
-export default CreateUser;
+}
