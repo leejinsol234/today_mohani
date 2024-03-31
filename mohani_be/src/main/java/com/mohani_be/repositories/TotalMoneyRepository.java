844f023011827b901be34f69ab7c36cc84d899b3
hilim9 <dlagydls1212@naver.com>
2024-04-01 03:44:12 +0900

    총 수입, 총 지출 수정 (월 단위로 조회) 가계부 변수명 추가 (month,year)

--- mohani_be/src/main/java/com/mohani_be/repositories/TotalMoneyRepository.java
+++ mohani_be/src/main/java/com/mohani_be/repositories/TotalMoneyRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TotalMoneyRepository extends JpaRepository<TotalMoney, Long> {
-    TotalMoney findByDateAndMember_MemberNo(String date, Long memberNo);
+
+    TotalMoney findByMonthAndYearAndMember_MemberNo(int month, int year, Long memberNo);
 
     List<TotalMoney> findByMember_MemberNo(Long memberNo);
 
-    Optional<TotalMoney> findByDate(String date);
 }
